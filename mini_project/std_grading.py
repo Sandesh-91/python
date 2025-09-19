6c0d9f0ac6f49eea4942e011a45af5fa8903e1dd
Sandesh Aryal <sandesharyal2062@gmail.com>
2025-09-19 19:10:51 +0545

    updated

--- mini_project/std_grading.py
+++ mini_project/std_grading.py
@@ -1,7 +1,7 @@
 ds=float(input("Enter marks in discrete structure: "))
 mth=float(input("Enter marks in mathematics-II : "))
 oop=float(input("Enter marks in OOPS : "))
-mp=float(input("Enter marks in micro processand: "))
+mp=float(input("Enter marks in micro processor: "))
 st=float(input("Enter marks in statistics : "))
 
 def calculateGpa(m):
@@ -24,9 +24,18 @@ gp_of_math=calculateGpa(mth)*3
 gp_of_oop=calculateGpa(oop)*3
 gp_of_stat=calculateGpa(st)*3
 gp_of_mp=calculateGpa(mp)*3
-
+print("\n")
 CGPA=(float)(gp_of_ds+gp_of_math+gp_of_mp+gp_of_oop+gp_of_stat)/15
 if CGPA==0:
     print("You have failed: ")
 else:
-    print(f"Your CGPA is {CGPA:.2f}")
+    
+    print("-------------------------------------RESULT-------------------------------------\n")
+    print(f"{"Subjects":<19}\t\t\t{"Credit Hours":^12}\t\t\t{"GPA":-^5}")
+    print(f"{"Discrete Structure":<19}\t\t\t{"3":^12}\t\t\t{gp_of_ds:>5.2f}")
+    print(f"{"Mathematics-II":<19}\t\t\t{"3":^12}\t\t\t{gp_of_math:>5.2f}")
+    print(f"{"OOP":<19}\t\t\t{"3":^12}\t\t\t{gp_of_oop:>5.2f}")
+    print(f"{"Statistics-I":<19}\t\t\t{"3":^12}\t\t\t{gp_of_stat:>5.2f}")
+    print(f"{"Micro Processor":<19}\t\t\t{"3":^12}\t\t\t{gp_of_mp:>5.2f}")
+
+    print(f"\nYour CGPA is {CGPA:.2f}")
