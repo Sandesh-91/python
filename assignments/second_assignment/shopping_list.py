16745a021eb2b7e50262b178119193573b042a9f
Sandesh Aryal <sandesharyal2062@gmail.com>
2025-09-21 19:15:56 +0545

    added

--- assignments/second_assignment/shopping_list.py
+++ assignments/second_assignment/shopping_list.py
@@ -3,13 +3,14 @@ def addItem():
     name=input("Name of product ")
     price=float(input("Enter price of product "))
     quantity=str(input("Enter quantity of product "))
-    shoppingList[name]={"Name":name, "price":price,"quantity":quantity}
+    shoppingList[name]={"Price":price,"Quantity":quantity}
 def viewItem():
     if not shoppingList:
         print("Shopping List is empty ")
-        print("\n Items in your List are: ")
+        return 
+    print("\nItems in your List are: ")
     for name,details in shoppingList.items():
-        print(f"Name: {name} , Price: Rs.{details["price"]}, Quantity: {details["quantity"]}")
+        print(f"Name: {name} , Price: Rs.{details['Price']}, Quantity: {details['Quantity']}")
 def removeItem():
     name=input("Enter the name of item that you want to remove: ")
     if name in shoppingList:
